27f8ce022430093ad02dedee731fcf31d98449ec
Sarmat Arsoev <84446010+SadButTruee@users.noreply.github.com>
2025-04-08 19:23:14 +0300

    Time: 0 ms (100%), Space: 40.3 MB (81.61%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.java
+++ 0070-climbing-stairs/0070-climbing-stairs.java
@@ -1,21 +1,19 @@
 class Solution {
     public int climbStairs(int n) {
-        int arr[] = new int[n + 1];
-        return f(arr, n);
+        if (n <= 2) {
+            return n;
+        }
+        return f(n);
     }
 
-    public int f(int arr[], int n) {
-        if (n == 1) {
-            return 1;
-        }
-        if (n == 2) {
-            return 2;
-        }
+    public int f(int n) {
 
-        if (arr[n] != 0) {
-            return arr[n];
+        int arr[] = new int[n + 1];
+        arr[1] = 1;
+        arr[2] = 2;
+        for (int i = 3; i <= n; i++) {
+            arr[i] = arr[i - 1] + arr[i - 2];
         }
-        arr[n] = f(arr, n - 1) + f(arr, n - 2) ;
         return arr[n];
     }
 }
